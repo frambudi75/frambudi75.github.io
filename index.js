c0e90c6c9d08d5b5cf06abd8fc757cdea03bff2c
Habib Frambudi <59114869+frambudi75@users.noreply.github.com>
2023-12-03 12:37:23 +0700

    p

--- index.js
+++ index.js
@@ -40,4 +40,32 @@ window.addEventListener("scroll", () => {
     isBackToTopRendered = false;
     alterStyles(isBackToTopRendered);
   }
+
+
+  // Mendapatkan semua elemen .work__box
+const workBoxes = document.querySelectorAll('.work__box');
+
+// Fungsi untuk memeriksa apakah elemen masuk dalam viewport
+function checkVisibility(element) {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    return elementTop < windowHeight;
+}
+
+// Memeriksa setiap elemen saat halaman discroll
+function handleScroll() {
+    workBoxes.forEach(box => {
+        if (checkVisibility(box)) {
+            box.classList.add('animate');
+        }
+    });
+}
+
+// Mendengarkan event scroll
+window.addEventListener('scroll', handleScroll);
+
+// Menjalankan sekali saat halaman dimuat untuk memeriksa elemen yang sudah ada di viewport
+handleScroll();
+
 });
